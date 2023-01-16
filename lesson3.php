7435bf49e751ff72acb414da8e4e97a4ec377d8b
misternogood <114906700+misternogood@users.noreply.github.com>
2023-01-16 16:21:28 +0900

    lesson2.3

--- lesson3.php
+++ lesson3.php
@@ -8,3 +8,12 @@
 // 20歳・女性
 
 // という形で出力してください。
+  $myWife = [
+    'name' => '山田',
+    'age' => '20',
+    'gender' => '女性'
+  ];
+
+  echo $myWife['name'];
+  echo "{$myWife['age']}歳・{$myWife['gender']}";
+  ?>
